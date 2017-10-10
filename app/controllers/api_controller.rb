caf24d5b77702d256e96695bf35032db58150919
Arthur Philadelpho <arthur22rj@gmail.com>
2017-10-10 17:23:35 -0300

    Changed @current_user variable name to @current_user_id.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -6,7 +6,7 @@
   def authenticated?
     authenticate_or_request_with_http_basic {|username, password| 
 			User.where( username: username, password: password).present? 
-			@current_user = User.where(username: username).pluck(:id).first
+			@current_user_id = User.where(username: username).pluck(:id).first
     }
     
   end
