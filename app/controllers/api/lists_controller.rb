35fbd9aa83703b042847b67d55c24e4e87d98b93
Arthur Philadelpho <arthur22rj@gmail.com>
2017-10-10 17:32:38 -0300

    Modified @current_user call in lists_controller to @current_user_id.

--- app/controllers/api/lists_controller.rb
+++ app/controllers/api/lists_controller.rb
@@ -3,7 +3,7 @@ class Api::ListsController < ApiController
  
   def create
   	list = List.new(list_params)
-    list.user_id = @current_user
+    list.user_id = @current_user_id
   	if list.save
   		render json: list
   	else
